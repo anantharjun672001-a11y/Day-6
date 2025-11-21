b33c363cb82951765074929245ea840dbee96ee8
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-11-21 19:03:32 +0530

    Example without map

---
+++ script.js
@@ -0,0 +1,20 @@
+ //Map :- 
+ //syntax :-  arrayname.map(function)
+
+ //Example without map : multiply the given array by 2
+
+
+/*steps:- 
+1. For loop
+2. multiply element by 2
+3. create a new array and push to it
+4. print new array
+
+
+const arr = [12, 13, 14, 15];
+const result = [];
+for (let i = 0; i < arr.length; i++) {
+  result.push(arr[i] * 2);
+}
+console.log(result);  */
+
